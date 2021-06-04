7b296b29401360ac56ddd21b5c742e8fba32835d
seifeldinahmed731 <83872282+seifeldinahmed731@users.noreply.github.com>
2021-06-04 14:42:02 +0200

    Update and rename menu to menu.php

--- code/menu.php
+++ code/menu.php
@@ -33,3 +33,24 @@ require_once("classes.php");
   	foreach ($allProducts as $product ) {?>
   		
   		<form method="post" action="menu.php?action=add&id=<?php echo$row["id"];?>">
+			<?php 
+  		
+  		echo"<div class='main'> ";
+  		echo"Drink name: ";
+  		echo $product->name;echo"<br>";
+		echo"Drink price: ";echo $product->StandardPrice;echo"<br>";	
+		echo"Drink description: ";echo $product->Description;	
+		echo"<br><br>";
+		echo "<img style='width: 150px; height: 150.5px; border-radius: 50px;  margin-top: -150px; margin-left:-450px; position:absolute; ' src= '$product->pic' >";
+  		
+
+		echo"<button style=' border-radius: 50px; padding: 10px; margin-top: -30px; margin-left:-40px; position:absolute; ' type='submit' name='addtocart'>Add to cart</button>";
+		echo"</div>";
+  	}
+
+  ?>
+  </form>
+</div>
+
+</html>  
+  
